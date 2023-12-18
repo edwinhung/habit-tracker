f97c151549788f822fb564ce0e48f706d201a8c9
Edwin Hung <32249535+edwinhung@users.noreply.github.com>
2023-12-17 22:47:47 -0800

    create basic mockup todo list

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,14 +1,35 @@
+import { useState } from 'react'
 
 function App() {
-  const todoList = ['meditate', 'cold shower', 'read']
+  const [newItem, setNewItem] = useState('');
+  const [todoList, setTodoList] = useState([])
+  const today = new Date()
+
+  const handleAdd = () => {
+    setTodoList(todoList.concat(newItem))
+  }
+
+  const handleClick = (event) => {
+    setNewItem(event.target.value)
+  }
 
   return (
-    <div>
-      <ul>
-        {todoList.map((item) => (
-          <li key={item}>{item}</li>
-        ))}
-      </ul>
+    <div className="container">
+      <div className="date">Today is {today.toDateString()}</div>
+      <div className="add">
+        <input type="text" onChange={handleClick} />
+        <button onClick={handleAdd}>Add</button>
+      </div>
+      <div className="list">
+        <ul>
+          {todoList.map((item) => (
+            <li key={item} className="item">
+              <input type="checkbox" className="checkbox" />
+              <label>{item}</label>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
